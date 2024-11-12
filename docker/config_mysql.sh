9157fbf931476665d5a4698afca3b7ecf1c00de5
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-11-12 12:50:16 +0100

    mysql install script: give permissions on *

--- docker/config_mysql.sh
+++ docker/config_mysql.sh
@@ -3,5 +3,8 @@
 # Change MySQL root password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'toor';"
 
+# Give permissions
+mysql --password=toor -e "grant all privileges on *.* to root@'localhost' with grant option;"
+
 # Configure MySQL with default options
-mysql_secure_installation -D -password=toor
+mysql_secure_installation -D --password=toor
