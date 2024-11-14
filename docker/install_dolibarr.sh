a97bb4fe3821b6f19c2ae45f09db8efdd26c1d41
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-11-14 14:45:15 +0100

    improve dolibarr install script

--- docker/install_dolibarr.sh
+++ docker/install_dolibarr.sh
@@ -2,8 +2,17 @@
 
 cd /var/www/html
 
+# Get the application from git repository
 git clone --depth 1 -b 20.0 http://github.com/Dolibarr/dolibarr.git dolibarr
 
+# Change permissions and owner of the 'dolibarr' directory
 chown -R www-data.www-data /var/www/html/dolibarr
 
+# create empty configuration file
+cd dolibarr ; touch htdocs/conf/conf.php ; chown www-data htdocs/conf/conf.php
 
+# directory that will be used to save all files generated and stocked by Dolibarr (PDf invoices, uploaded images, ...)
+mkdir -p /var/lib/dolibarr/documents ; chown www-data /var/lib/dolibarr/documents
+
+# lock the call of the install process again
+touch /var/lib/dolibarr/documents/install.lock; chmod go-w /var/lib/dolibarr/documents
