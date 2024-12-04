ed6019aecd5172083c429f1cf3188d73365acd5a
marccambon <marccambonm@gmail.com>
2024-12-04 15:51:37 +0100

    aaa

--- docker/csv.sh
+++ docker/csv.sh
@@ -1,4 +1,6 @@
-LOAD DATA INFILE 'test-import.csv' INTO TABLE llx_facture
-  FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-  LINES TERMINATED BY '\r\n'
-  IGNORE 1 LINES;
+#!/usr/bin/env bash
+
+mysql -e "LOAD DATA INFILE 'test-import.csv' INTO TABLE llx_facture \
+  FIELDS TERMINATED BY ',' ENCLOSED BY '"' \
+  LINES TERMINATED BY '\r\n' \
+  IGNORE 1 LINES;"
