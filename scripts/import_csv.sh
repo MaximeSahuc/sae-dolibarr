352cf377e731706ef7b3219d807a50f1541f75fe
marccambon <marccambonm@gmail.com>
2024-12-04 18:57:07 +0100

    test import csv

--- scripts/import_csv.sh
+++ scripts/import_csv.sh
@@ -7,9 +7,26 @@ DB_HOST="dolibarr_mariadb"
 DB_NAME="dolibarr"
 DOLIBARR_CONTAINER="dolibarr_web"
 
-CMD="mysql --database $DB_NAME -h $DB_HOST -u $DB_USER -p$DB_PASS -e \"LOAD DATA INFILE '/root/test-import.csv' INTO TABLE llx_facture FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;\""
-
+# sudo docker exec \
+#     $DOLIBARR_CONTAINER \
+#     bash -c \
+#         "mysql \
+#         --database $DB_NAME \
+#         -h $DB_HOST \
+#         -u $DB_USER \
+#         -p$DB_PASS \
+#         -e "LOAD DATA INFILE '/root/test-import.csv' INTO TABLE llx_facture \
+#         FIELDS TERMINATED BY ',' ENCLOSED BY '"' \
+#         LINES TERMINATED BY '\r\n' \
+#         IGNORE 1 LINES;""
 
 sudo docker exec \
-  $DOLIBARR_CONTAINER \
-  bash -c $CMD
+    $DOLIBARR_CONTAINER \
+    bash -c "mysql \
+    --database dolibarr \
+    -h dolibarr_mariadb \
+    -u root \
+    -proot \
+    -e "LOAD DATA LOCAL INFILE '/root/csv_import/test-import.csv' INTO TABLE llx_user \
+    FIELDS TERMINATED BY ',' ENCLOSED BY '\"' \
+    LINES TERMINATED BY '\n' IGNORE 1 LINES;""
