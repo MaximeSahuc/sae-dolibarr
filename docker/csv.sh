34c43996fe99b742d11888ac6e28b97f5650cebf
marccambon <marccambonm@gmail.com>
2024-12-04 17:20:20 +0100

    bbb

--- docker/csv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -e "LOAD DATA INFILE 'test-import.csv' INTO TABLE llx_facture \
-  FIELDS TERMINATED BY ',' ENCLOSED BY '"' \
-  LINES TERMINATED BY '\r\n' \
-  IGNORE 1 LINES;"
