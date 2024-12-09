fd1d6f095c40fcca94969339e4e284452ca8a283
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-12-09 19:02:27 +0100

    refacto

--- scripts/import_csv.py
+++ scripts/import_csv.py
@@ -7,7 +7,7 @@ API_URL = "http://0.0.0.0/api/index.php/users"
 API_KEY = "1Q2b68bUAQohJg9iuh4MXqK1q91XH8Fm"
 
 # Import users list via HTTP requests
-with open('../csv_import/test_users.csv', newline='') as csvfile:
+with open('../imports/test_users.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         user_data = {
@@ -29,3 +29,5 @@ with open('../csv_import/test_users.csv', newline='') as csvfile:
         
         if response.status_code != 200:
             print(f"Error: Failed to create user {row['login']}.\nResponse: {response.text}")
+
+print("Done!")
